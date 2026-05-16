62f239ac52f003d8bfb26b0f198e50f89d573653
RoRReT <55076039+rORrEtboLt@users.noreply.github.com>
2026-05-16 13:54:23 +0530

    ios ipad install working (#8)
    Co-authored-by: Nesara Hippla <nesarahippla@Mac.lan>

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.islanddefence.app',
+  appName: 'island-defence',
+  webDir: 'dist'
+};
+
+export default config;
